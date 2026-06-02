70104b0495ff6ac87038209bac4cfe838e2a6c5a
Antonio Sitoe <antoniositoehl@gmail.com>
2026-06-02 15:59:34 +0200

    ajustes

---
+++ frontend/src/app/pages/costumers/costumers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+import { PageHeaderComponent } from "@/shared/components/common/page-header/page-header.component";
+import { CardStatComponent } from "@/shared/components/common/card-stat/card-stat.component";
+
+@Component({
+  selector: "app-costumers",
+  imports: [PageHeaderComponent, CardStatComponent],
+  templateUrl: "./costumers.component.html",
+  styles: ``,
+})
+export class CostumersComponent {}
