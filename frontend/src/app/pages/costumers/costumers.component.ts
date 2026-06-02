27b1735a6c8864501cc62ede87eb7c840cb91ce6
Antonio Sitoe <antoniositoehl@gmail.com>
2026-06-02 16:09:32 +0200

    ajustes

--- frontend/src/app/pages/costumers/costumers.component.ts
+++ frontend/src/app/pages/costumers/costumers.component.ts
@@ -1,5 +1,4 @@
 import { Component } from "@angular/core";
-
 import { PageHeaderComponent } from "@/shared/components/common/page-header/page-header.component";
 import { CardStatComponent } from "@/shared/components/common/card-stat/card-stat.component";
 
