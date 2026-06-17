717292cd953e8513443395c6c5fcdb4b8b0bb63e
Antonio Sitoe <antoniositoehl@gmail.com>
2026-06-17 09:46:24 +0200

    add a wrapper for a global search componente

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core'
-import { RouterModule } from '@angular/router'
-import { NgxSonnerToaster } from 'ngx-sonner'
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { NgxSonnerToaster } from "ngx-sonner";
+import { GlobalSearchWrapperComponent } from "./shared/components/global-search/global-search-wrapper/global-search-wrapper.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterModule, NgxSonnerToaster],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  imports: [RouterModule, NgxSonnerToaster, GlobalSearchWrapperComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'Finance App | A dashboard app to analyze your transactions'
+  title = "Finance App | A dashboard app to analyze your transactions";
 }
